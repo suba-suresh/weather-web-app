4a83189b9e6c4466aa0fc15d0bc1ab0f8925683b
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-07-02 12:09:03 +0000

    feat: :sparkles: move variables to function, add test html to test if function works

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@
 
 
 // computer weather to beat
-const computerWeatherArr = ['01d','02d','03d','04d','09d','10d','11d','13d','50d'];
-const randomComputerChoice = computerWeatherArr[Math.floor(Math.random()*9)];
-console.log(randomComputerChoice);
-const currConditionImage = `https://openweathermap.org/img/wn/${randomComputerChoice}@2x.png`;
+function makeComputerChoice() {
+    const computerWeatherArr = ['01d','02d','03d','04d','09d','10d','11d','13d','50d'];
+    const randomComputerChoice = computerWeatherArr[Math.floor(Math.random()*9)];
+    console.log(randomComputerChoice);
+    const computerChoiceImage = `https://openweathermap.org/img/wn/${randomComputerChoice}@2x.png`;
+    document.getElementById('computer-choice').innerHTML = `<img src="${computerChoiceImage}">`;
+}
