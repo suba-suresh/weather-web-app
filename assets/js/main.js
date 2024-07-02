db47730257226a8fdf935eb4a8091cb730f82729
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-07-02 11:54:56 +0000

    ci: :hammer: add JS randomised computer choice variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 
+
+
+// computer weather to beat
+const computerWeatherArr = ['01d','02d','03d','04d','09d','10d','11d','13d','50d'];
+const randomComputerChoice = computerWeatherArr[Math.floor(Math.random()*9)];
+console.log(randomComputerChoice);
+const currConditionImage = `https://openweathermap.org/img/wn/${randomComputerChoice}@2x.png`;
