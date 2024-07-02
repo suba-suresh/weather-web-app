3ec3318f99b0a9de813f2d7baf358dbef19d3b55
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-07-02 15:28:46 +0000

    feat: :sparkles: add function to next round button, add styling to footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,18 @@
 
-
-
+  
+  
+makeComputerChoice();
+document.getElementById("next-round").addEventListener("click", function(event) {
+    event.preventDefault();
+    makeComputerChoice();
+  });
 // computer weather to beat
+/** Randomly pick a weather icon and display it in the 'weather to beat' section */
 function makeComputerChoice() {
-    const computerWeatherArr = ['01d','02d','03d','04d','09d','10d','11d','13d','50d'];
-    const randomComputerChoice = computerWeatherArr[Math.floor(Math.random()*9)];
+    const computerWeatherArr = ['01d','02d','03d','04d','09d','10d'];
+    const randomComputerChoice = computerWeatherArr[Math.floor(Math.random()*6)];
     console.log(randomComputerChoice);
-    const computerChoiceImage = `https://openweathermap.org/img/wn/${randomComputerChoice}@2x.png`;
+    const computerChoiceImage = `https://openweathermap.org/img/wn/${randomComputerChoice}@4x.png`;
+    // document.getElementById('computer-default').classList.add("hidden");
     document.getElementById('computer-choice').innerHTML = `<img src="${computerChoiceImage}">`;
 }
